4f22ba0cac7fdbf1af72527fbd75160b7b8e4321
Mukul Anand <mukulanand.biz@gmail.com>
2024-11-28 18:15:17 +0530

    fix4: labid page

--- src/app/(dashboard)/lab/[labid]/page.tsx
+++ src/app/(dashboard)/lab/[labid]/page.tsx
@@ -1,8 +1,26 @@
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ labid: string }>;
-}) {
-  const labid = (await params).labid;
-  return <div>My Lab ID: {labid}</div>;
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface PageProps {
+  params: {
+    labid: string;
+  };
 }
+
+const Page = async ({ params }: PageProps) => {
+  const { labid } = params;
+  const user = await currentUser();
+
+  if (!user || !user.id) {
+    redirect(`/sign-in`);
+  }
+
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)">
+      <h1>Lab {labid}</h1>
+      <h2 className="text-2xl font-bold">Hey, {user.firstName}</h2>
+    </div>
+  );
+};
+
+export default Page;
