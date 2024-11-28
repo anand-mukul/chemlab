de2d23ca4e723577a6f4dc6748b50746c3ff8e77
Mukul Anand <mukulanand.biz@gmail.com>
2024-11-28 18:40:05 +0530

    fix6: dynamic page

--- src/app/(dashboard)/lab/[id]/page.tsx
+++ src/app/(dashboard)/lab/[id]/page.tsx
@@ -1,14 +1,11 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-const Page = async ({ params }: Props) => {
-  const { id } = params;
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
   const user = await currentUser();
 
   if (!user || !user.id) {
@@ -16,11 +13,9 @@ const Page = async ({ params }: Props) => {
   }
 
   return (
-    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)">
-      <h1>Lab {id}</h1>
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <h1>Lab {params.id}</h1>
       <h2 className="text-2xl font-bold">Hey, {user.firstName}</h2>
     </div>
   );
-};
-
-export default Page;
+}
