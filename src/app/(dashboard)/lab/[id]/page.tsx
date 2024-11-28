672265dacb143f692599c900dcdd9c2bd12faeda
Mukul Anand <mukulanand.biz@gmail.com>
2024-11-28 18:42:26 +0530

    fix7: updated dynamic page

--- src/app/(dashboard)/lab/[id]/page.tsx
+++ src/app/(dashboard)/lab/[id]/page.tsx
@@ -4,17 +4,17 @@ import { redirect } from "next/navigation";
 export default async function Page({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const id = (await params).id;
   const user = await currentUser();
 
   if (!user || !user.id) {
     redirect(`/sign-in`);
   }
-
   return (
-    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
-      <h1>Lab {params.id}</h1>
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)">
+      <h1>Lab {id}</h1>
       <h2 className="text-2xl font-bold">Hey, {user.firstName}</h2>
     </div>
   );
