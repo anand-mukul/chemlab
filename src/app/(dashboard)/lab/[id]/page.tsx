3e9b142635aac3ef606629cd80eded886da26855
Mukul Anand <mukulanand.biz@gmail.com>
2024-11-28 18:29:45 +0530

    fix5: dynamic lab page added

--- src/app/(dashboard)/lab/[id]/page.tsx
+++ src/app/(dashboard)/lab/[id]/page.tsx
@@ -1,14 +1,14 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-interface PageProps {
+type Props = {
   params: {
-    labid: string;
+    id: string;
   };
-}
+};
 
-const Page = async ({ params }: PageProps) => {
-  const { labid } = params;
+const Page = async ({ params }: Props) => {
+  const { id } = params;
   const user = await currentUser();
 
   if (!user || !user.id) {
@@ -17,7 +17,7 @@ const Page = async ({ params }: PageProps) => {
 
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)">
-      <h1>Lab {labid}</h1>
+      <h1>Lab {id}</h1>
       <h2 className="text-2xl font-bold">Hey, {user.firstName}</h2>
     </div>
   );
